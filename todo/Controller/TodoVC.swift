6a0f39285e663237e0cfffad363e34a8aa5b4cb8
VNavrockij <vitalik.nav@gmail.com>
2022-05-25 10:09:49 +0300

    add: dislaying data in table

--- todo/Controller/TodoVC.swift
+++ todo/Controller/TodoVC.swift
@@ -7,22 +7,55 @@
 
 import UIKit
 
-class TodoVC: UIViewController {
+class TodoVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var todoItemTxt: UITextField!
     @IBOutlet weak var prioritySegment: UISegmentedControl!
+    @IBOutlet weak var todoTable: UITableView!
     
-    
+    var todos = Array<Todo>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        todoTable.delegate = self
+        todoTable.dataSource = self
+        
+        getTodos()
+
+    }
+    
+    func getTodos() {
         NetworkService.shared.getTodos { (todos) in
+            self.todos = todos.items
+            self.todoTable.reloadData()
             debugPrint(todos)
         }
     }
 
     @IBAction func addTodo(_ sender: Any) {
+    
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell") as? TodoCell {
+            cell.updateCell(todo: todos[indexPath.row])
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todos.count
     }
     
 }
--- todo/View/TodoCell.swift
+++ todo/View/TodoCell.swift
@@ -13,15 +13,19 @@ class TodoCell: UITableViewCell {
     @IBOutlet weak var priorityView: UIView!
     
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func updateCell(todo: Todo) {
+        itemLbl.text = todo.item
+        
+        switch todo.priority {
+        case 0:
+            priorityView.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+            break
+        case 1:
+            priorityView.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            break
+        default:
+            priorityView.backgroundColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
+        }
     }
 
 }
